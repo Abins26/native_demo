58db6b136b7afa1748291e4d5b3e8c725a397c3c
Abins26 <abinsms26@gmail.com>
2024-05-16 18:35:19 +0530

    1.002

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import disney from "./assets/disney";
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import disney from "./assets/disney.png";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello Abins</Text>
-      <Image style={styles.logo} source={disney}/>
+      <Text style={styles.text}>Welcome User</Text>
       <StatusBar style="auto" />
+      <Image style={styles.logo} source={require("./assets/disney.png" )}/>
+      <Image style={styles.logo} source={disney}/>
+      {/* <Image style={styles.reactlogo} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png',}}/> */}
+
     </View>
   );
 }
@@ -15,12 +17,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    color:'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'red',
+    fontSize:25,
+    fontWeight:'800'
+  },
   logo: {
+    display:"flex",
     height: 200,
     width: 150,
+  },
+  reactlogo: {
+
+    height:200,
+    width:2
   }
+
 });
